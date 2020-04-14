eae2fb38b9a3fbc7dea7f114d6c542683f0df5b5
rap9210 <rap9210@gmail.com>
2020-04-14 12:19:02 -0400

    Complete Initialization

---
+++ src/CompInit.java
@@ -0,0 +1,12 @@
+public class CompInit {
+    public static void main(String[] args){
+        int workOutDays = 10;
+        double health = 8.5;
+        boolean powerUp = true;
+
+        System.out.println("workOutDays: "+ workOutDays);
+        System.out.println("health: "+ health);
+        System.out.println("powerUp: "+ powerUp);
+        System.out.println(workOutDays +","+ health +","+ powerUp);
+    }
+}
